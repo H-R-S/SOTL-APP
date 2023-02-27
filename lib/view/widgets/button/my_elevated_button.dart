85760420dac10e3c7732fcb87bc4274b773c57b3
H-R-S <369haris@gmail.com>
2023-02-27 21:22:02 +0500

    Widget UI updated | MyElevatedButton Widget

---
+++ lib/view/widgets/button/my_elevated_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../resources/constants/style.dart';
+
+class MyElevatedButton extends StatelessWidget {
+  final String title;
+  final Function() onTap;
+  final double? customWidth;
+  final Color? buttonColor;
+  final Color? contentColor;
+  final bool isLoading;
+
+  const MyElevatedButton(
+      {super.key,
+      required this.title,
+      required this.onTap,
+      this.customWidth,
+      this.isLoading = false,
+      this.contentColor = Colors.white,
+      this.buttonColor = primary});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: onTap,
+        child: Container(
+          width: customWidth ?? double.infinity,
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: buttonColor),
+            child: Center(
+              child: isLoading
+                  ? SizedBox(
+                      height: 30,
+                      child: Center(
+                          child: CircularProgressIndicator(
+                              color: contentColor ?? Colors.white)))
+                  : Text(title,
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: contentColor,
+                          fontWeight: FontWeight.bold)),
+            )));
+  }
+}
