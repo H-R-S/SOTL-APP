bb7c7633cd384c7993cdf8e7f29da1a80978fea7
H-R-S <369haris@gmail.com>
2023-04-06 03:42:37 +0500

    Screen UI updated | Profile Screen

--- lib/view/screens/profile/profile_screen.dart
+++ lib/view/screens/profile/profile_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sotl/view/widgets/button/my_elevated_button.dart';
-import 'package:sotl/view/widgets/snack_bar/my_snack_bar.dart';
-
 import '../../widgets/app_bar/my_app_bar.dart';
+import '../../widgets/button/my_elevated_button.dart';
+import '../../widgets/snack_bar/my_snack_bar.dart';
 import '../../widgets/text_field/my_text_field.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -22,36 +21,35 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-          appBar:
-              MyAppBar(scaffoldKey, context, title: "Profile", isBack: true),
-          body: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(children: [
-                MyTextField(
-                    header: "Full Name",
-                    controller: nameController,
-                    hint: "Full Name"),
-                MyTextField(
-                    header: "Email",
-                    controller: emailController,
-                    hint: "Email"),
-                MyTextField(
-                    header: "Phone No.",
-                    controller: phoneNoController,
-                    hint: "Phone No."),
-                MyTextField(
-                    header: "DOB", controller: dobController, hint: "DOB"),
-                MyTextField(
-                    header: "Role", controller: roleController, hint: "Role"),
-                const SizedBox(height: 20),
-                MyElevatedButton(
-                    title: "Update",
-                    onTap: () {
-                      MySnackBar(context, "Info Updated");
-                    })
-              ]))),
-    );
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+            appBar:
+                MyAppBar(scaffoldKey, context, title: "Profile", isBack: true),
+            body: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(children: [
+                  MyTextField(
+                      header: "Full Name",
+                      controller: nameController,
+                      hint: "Full Name"),
+                  MyTextField(
+                      header: "Email",
+                      controller: emailController,
+                      hint: "Email"),
+                  MyTextField(
+                      header: "Phone No.",
+                      controller: phoneNoController,
+                      hint: "Phone No."),
+                  MyTextField(
+                      header: "DOB", controller: dobController, hint: "DOB"),
+                  MyTextField(
+                      header: "Role", controller: roleController, hint: "Role"),
+                  const SizedBox(height: 20),
+                  MyElevatedButton(
+                      title: "Update",
+                      onTap: () {
+                        MySnackBar(context, "Info Updated");
+                      })
+                ]))));
   }
 }
