2ef371874c70eea44bfc3642db631f0dd7f746d4
H-R-S <369haris@gmail.com>
2023-04-06 03:39:26 +0500

    Validation removed | Login Screen

--- lib/view/screens/login/login_screen.dart
+++ lib/view/screens/login/login_screen.dart
@@ -18,67 +18,70 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: SafeArea(
-        child: Scaffold(
-            body: SingleChildScrollView(
-                child: Form(
-                    key: formKey,
-                    child: Padding(
-                        padding: const EdgeInsets.all(20).copyWith(top: 50),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset(iqraLogo, height: 250),
-                              const SizedBox(height: 15),
-                              const Text("SOTL SYSTEM",
-                                  style: TextStyle(
-                                      color: primary,
-                                      fontSize: 32,
-                                      fontWeight: FontWeight.bold)),
-                              const SizedBox(height: 15),
-                              const Text("Please Sign-in to your account",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.normal)),
-                              const SizedBox(height: 15),
-                              MyTextField(
-                                  validator: (value) =>
-                                      Validator.validateEmail(value),
-                                  controller: emailController,
-                                  hint: "Email"),
-                              MyTextField(
-                                  validator: (value) =>
-                                      Validator.validatePassword(value),
-                                  isPassword: true,
-                                  isVisible: true,
-                                  controller: passwordController,
-                                  hint: "Password"),
-                              Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    TextButton(
-                                        onPressed: () {
-                                          Navigator.pushNamed(
-                                              context, RoutesName.forgotPassword);
-                                        },
-                                        child: const Text("Forgot Password?",
-                                            style: TextStyle(
-                                                fontSize: 16, color: primary)))
-                                  ]),
-                              const SizedBox(height: 15),
-                              MyElevatedButton(
-                                  onTap: () {
-                                    final isValidate =
-                                        formKey.currentState!.validate();
-      
-                                    if (isValidate) {
-                                      MySnackBar(context, "Login successfully");
-                                    }
-                                  },
-                                  title: "Login"),
-                            ]))))),
-      ),
-    );
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SafeArea(
+            child: Scaffold(
+                body: SingleChildScrollView(
+                    child: Form(
+                        key: formKey,
+                        child: Padding(
+                            padding: const EdgeInsets.all(20).copyWith(top: 50),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(iqraLogo, height: 250),
+                                  const SizedBox(height: 15),
+                                  const Text("SOTL SYSTEM",
+                                      style: TextStyle(
+                                          color: primary,
+                                          fontSize: 32,
+                                          fontWeight: FontWeight.bold)),
+                                  const SizedBox(height: 15),
+                                  const Text("Please Sign-in to your account",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.normal)),
+                                  const SizedBox(height: 15),
+                                  MyTextField(
+                                      // validator: (value) =>
+                                      //     Validator.validateEmail(value),
+                                      controller: emailController,
+                                      hint: "Email"),
+                                  MyTextField(
+                                      // validator: (value) =>
+                                      //     Validator.validatePassword(value),
+                                      isPassword: true,
+                                      isVisible: true,
+                                      controller: passwordController,
+                                      hint: "Password"),
+                                  Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        TextButton(
+                                            onPressed: () {
+                                              Navigator.pushNamed(context,
+                                                  RoutesName.forgotPassword);
+                                            },
+                                            child: const Text(
+                                                "Forgot Password?",
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: primary)))
+                                      ]),
+                                  const SizedBox(height: 15),
+                                  MyElevatedButton(
+                                      onTap: () {
+                                        // final isValidate =
+                                        //     formKey.currentState!.validate();
+
+                                        // if (isValidate) {
+                                          MySnackBar(
+                                              context, "Login successfully");
+                                          Navigator.pushReplacementNamed(
+                                              context, RoutesName.home);
+                                        // }
+                                      },
+                                      title: "Login")
+                                ])))))));
   }
 }
