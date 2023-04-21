ba792c3bd4e2cc984c822fdb9f11de4c14b56ae9
H-R-S <369haris@gmail.com>
2023-04-21 23:38:47 +0500

    New color's added | Style Constants

--- lib/resources/constants/style.dart
+++ lib/resources/constants/style.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 const Color primary = Color(0xff040B5B);
+const Color active = Color(0xffFCC43E);
+const Color secondary = Color(0xffADB3BC);
+const Color light = Color(0xffF3F4FF);
