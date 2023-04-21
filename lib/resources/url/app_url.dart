300d968aa5d2c5dcbeb0611a60185a4cabc5bc9c
H-R-S <369haris@gmail.com>
2023-04-21 23:37:52 +0500

    New URL's added | AppUrl

---
+++ lib/resources/url/app_url.dart
@@ -0,0 +1,12 @@
+class AppUrl {
+
+  static var baseUrl = "";
+
+  static var loginEndPoint = "$baseUrl/api/login";
+
+  static var creatUserEndPoint = "$baseUrl/api/create";
+
+  static var getAllUsersEndPoint = "$baseUrl/api/users";
+
+  static var updateUserEndPoint = "$baseUrl/api/update";
+}
