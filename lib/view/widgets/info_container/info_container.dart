a7ec2cde9887556f0cee0e4dd38636a6f6b7efad
H-R-S <369haris@gmail.com>
2023-04-25 21:30:50 +0500

    Widget UI updated | InfoContainer Widget

--- lib/view/widgets/info_container/info_container.dart
+++ lib/view/widgets/info_container/info_container.dart
@@ -5,34 +5,42 @@ class InfoContainer extends StatelessWidget {
   final String title;
   final String value;
   final Color color;
+  final Function() onTap;
 
   const InfoContainer(
       {super.key,
       required this.title,
       required this.value,
+      required this.onTap,
       this.color = primary});
 
   @override
   Widget build(BuildContext context) {
     const style = TextStyle(fontSize: 20, color: Colors.white);
 
-    return Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30), color: color),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          const CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Icon(Icons.supervisor_account_sharp, color: primary)),
-          FittedBox(
-              child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                Text(title, overflow: TextOverflow.ellipsis, style: style),
-                Text(value, overflow: TextOverflow.ellipsis, style: style)
-              ]))
-        ]));
+    return InkWell(
+        onTap: onTap,
+        child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30), color: color),
+            child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.supervisor_account_sharp, color: primary)),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: style),
+                      Text(value, overflow: TextOverflow.ellipsis, style: style)
+                    ]),
+              )
+            ])));
   }
 }
