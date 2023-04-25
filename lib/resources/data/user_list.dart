d8b0fa18e22a6125cc6573e1da947b301e852325
H-R-S <369haris@gmail.com>
2023-04-25 21:33:18 +0500

    Demo data added | Data

---
+++ lib/resources/data/user_list.dart
@@ -0,0 +1,49 @@
+List facultyList = [
+  {
+    "name": "Campus Directors",
+    "value": 32
+  },
+  {
+    "name": "Deans",
+    "value": 32
+  },
+  {
+    "name": "Head of Departments",
+    "value": 32
+  },
+  {
+    "name": "Observers",
+    "value": 32
+  },
+  {
+    "name": "Faculty",
+    "value": 32
+  },
+  {
+    "name": "Guest",
+    "value": 32
+  }
+];
+
+List userList = [
+    {
+      "name": "Sheraz",
+      "designation": "Mathematics",
+      "email": "sheraz.ahmed@iqra.edu.pk"
+    },
+    {
+      "name": "Haris",
+      "designation": "Statistics",
+      "email": "muhammad.haris@iqra.edu.pk"
+    },
+    {
+      "name": "Krish",
+      "designation": "Data Structure",
+      "email": "krish.21412@iqra.edu.pk"
+    },
+    {
+      "name": "Bilal",
+      "designation": "Mathematics",
+      "email": "bilal.ahmed@iqra.edu.pk"
+    },
+  ];
