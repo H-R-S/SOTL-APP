181e763358625e5ad92aa6e566352ac404e836d7
H-R-S <369haris@gmail.com>
2023-04-25 21:29:11 +0500

    Screen UI updated | Users List Screen

---
+++ lib/view/screens/users_list/users_list_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import '../../../resources/constants/style.dart';
+import '../../../resources/data/user_list.dart';
+import '../../widgets/app_bar/my_app_bar.dart';
+import '../../widgets/header_tag/header_tag.dart';
+import '../../widgets/search_bar/my_search_bar.dart';
+import '../../widgets/user_card/user_card.dart';
+
+class UsersListScreen extends StatelessWidget {
+  final String? title;
+
+  UsersListScreen({super.key, required this.title});
+
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: light,
+        appBar: MyAppBar(scaffoldKey, context,
+            isBack: true,
+            backButtonColor: primary,
+            backgroundColor: Colors.transparent),
+        body: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      HeaderTag(title: title ?? ""),
+                      Container(
+                          decoration: BoxDecoration(
+                              color: primary,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.add, color: Colors.white)))
+                    ]),
+                const SizedBox(height: 20),
+                MySearchBar(
+                    onTapSufix: () {},
+                    sufixIcon: Icons.filter_list_rounded,
+                    hint: "Search $title",
+                    controller: searchController,
+                    onChanged: (value) {}),
+                const SizedBox(height: 40),
+                GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            mainAxisExtent: 200,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 20,
+                            crossAxisCount: 2),
+                    itemCount: userList.length,
+                    itemBuilder: (context, index) {
+                      return UserCard(
+                          onTap: () {},
+                          name: userList[index]["name"],
+                          designation: userList[index]["designation"],
+                          email: userList[index]["email"]);
+                    })
+              ])),
+        ));
+  }
+}
