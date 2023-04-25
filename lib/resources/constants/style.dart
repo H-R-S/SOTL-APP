63836e9df4d179fca3ca9042fa7a81ae2061f571
H-R-S <369haris@gmail.com>
2023-04-25 21:32:53 +0500

    New colors added | Style Constants

--- lib/resources/constants/style.dart
+++ lib/resources/constants/style.dart
@@ -4,3 +4,4 @@ const Color primary = Color(0xff040B5B);
 const Color active = Color(0xffFCC43E);
 const Color secondary = Color(0xffADB3BC);
 const Color light = Color(0xffF3F4FF);
+const Color grey = Color(0xffA098AE);
