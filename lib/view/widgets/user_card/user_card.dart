79f92a13b29f246749a798e4a21901c2778a53bf
H-R-S <369haris@gmail.com>
2023-04-25 21:30:15 +0500

    Widget UI updated | UserCard Widget

---
+++ lib/view/widgets/user_card/user_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../../../resources/constants/style.dart';
+
+class UserCard extends StatelessWidget {
+  final String name, designation, email;
+  final Function() onTap;
+
+  const UserCard(
+      {super.key,
+      required this.onTap,
+      required this.name,
+      required this.designation,
+      required this.email});
+
+  @override
+  Widget build(BuildContext context) {
+    const style =
+        TextStyle(color: primary, fontSize: 20, fontWeight: FontWeight.bold);
+
+    return Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(boxShadow: const [
+          BoxShadow(
+              offset: Offset(1, 4),
+              spreadRadius: 2,
+              blurRadius: 2,
+              color: secondary)
+        ], color: Colors.white, borderRadius: BorderRadius.circular(10)),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const CircleAvatar(
+              radius: 40,
+              backgroundColor: primary,
+              child: Icon(Icons.person, size: 60, color: Colors.white)),
+          const SizedBox(height: 10),
+          Text(name, style: style),
+          const SizedBox(height: 10),
+          Text(designation,
+              style: style.copyWith(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                  color: secondary)),
+          const SizedBox(height: 10),
+          FittedBox(child: Text(email, style: style))
+        ]));
+  }
+}
