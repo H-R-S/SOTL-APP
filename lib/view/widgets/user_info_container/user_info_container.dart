c46f5bb94d6264aa95c69060c07fe34d575b137b
H-R-S <369haris@gmail.com>
2023-04-25 21:30:36 +0500

    Widget UI updated | UserInfoContainer Widget

--- lib/view/widgets/user_info_container/user_info_container.dart
+++ lib/view/widgets/user_info_container/user_info_container.dart
@@ -20,25 +20,26 @@ class UserInfoContainer extends StatelessWidget {
         fontSize: 20, color: Colors.white, fontWeight: FontWeight.normal);
 
     return InkWell(
-      onTap: onTap,
-      child: Container(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30), color: primary),
-          child: Row(children: [
-            const CircleAvatar(
-                radius: 30,
-                backgroundColor: Colors.white,
-                child: Icon(Icons.person, color: primary, size: 30)),
-            const SizedBox(width: 20),
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(name, style: style.copyWith(fontWeight: FontWeight.bold)),
-              const SizedBox(height: 5),
-              Text(email, style: style.copyWith(color: Colors.white54)),
-              const SizedBox(height: 5),
-              Text(role, style: style)
-            ])
-          ])),
-    );
+        onTap: onTap,
+        child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30), color: primary),
+            child: Row(children: [
+              const CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.person, color: primary, size: 30)),
+              const SizedBox(width: 20),
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Text(name, style: style.copyWith(fontWeight: FontWeight.bold)),
+                const SizedBox(height: 5),
+                FittedBox(
+                    child: Text(email,
+                        style: style.copyWith(color: Colors.white54))),
+                const SizedBox(height: 5),
+                Text(role, style: style)
+              ])
+            ])));
   }
 }
