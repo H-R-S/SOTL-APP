efade8ce0ad5311b65c04825575e5ff2b86fe4d7
H-R-S <369haris@gmail.com>
2023-04-25 21:29:55 +0500

    Widget UI updated | MySearchBar Widget

---
+++ lib/view/widgets/search_bar/my_search_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../../../resources/constants/style.dart';
+
+class MySearchBar extends StatelessWidget {
+  final String? Function(String?)? validator;
+  final Function()? onTap;
+  final String hint;
+  final bool isReadOnly;
+  final Color filledColor;
+  final IconData prefixIcon;
+  final TextEditingController controller;
+  final Function(String) onChanged;
+  final IconData? sufixIcon;
+  final Function()? onTapSufix;
+
+  const MySearchBar(
+      {super.key,
+      this.validator,
+      this.onTap,
+      required this.hint,
+      this.filledColor = Colors.white,
+      this.isReadOnly = false,
+      this.prefixIcon = Icons.search,
+      required this.controller,
+      required this.onChanged,
+      this.sufixIcon,
+      this.onTapSufix});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        validator: validator,
+        readOnly: isReadOnly,
+        onTap: onTap,
+        onChanged: onChanged,
+        controller: controller,
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: filledColor,
+            prefixIcon: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 10),
+                child: Icon(prefixIcon, color: grey)),
+            suffixIcon: sufixIcon != null
+                ? Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: InkWell(
+                        onTap: onTapSufix, child: Icon(sufixIcon, color: grey)),
+                  )
+                : null,
+            hintText: hint,
+            enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)),
+            border: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(30))));
+  }
+}
