8ab0797167bfd56639e35a0840bca3a417301fe7
H-R-S <369haris@gmail.com>
2023-04-28 20:08:22 +0500

    Screen UI updated | Users Screen

--- lib/view/screens/users/users_screen.dart
+++ lib/view/screens/users/users_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../../resources/constants/style.dart';
 import '../../../resources/data/user_list.dart';
+import '../../widgets/app_bar/my_app_bar.dart';
 import '../../widgets/header_tag/header_tag.dart';
 import '../../widgets/info_container/info_container.dart';
 import '../users_list/users_list_screen.dart';
@@ -13,33 +14,31 @@ class UsersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: light,
-        // appBar:
-        //     MyAppBar(scaffoldKey, context, backgroundColor: Colors.transparent),
+        appBar: MyAppBar(scaffoldKey, context, title: "Users"),
         body: SingleChildScrollView(
             child: Padding(
-                padding: const EdgeInsets.all(20).copyWith(top: 50),
+                padding: const EdgeInsets.all(20),
                 child: Column(children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const HeaderTag(title: "Users"),
-                        Container(
-                            decoration: BoxDecoration(
-                                color: primary,
-                                borderRadius: BorderRadius.circular(10)),
-                            child: IconButton(
-                                onPressed: () {},
-                                icon:
-                                    const Icon(Icons.add, color: Colors.white)))
-                      ]),
-                  const SizedBox(height: 40),
+                  // Row(
+                  //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //     children: [
+                  //       const HeaderTag(title: "Users"),
+                  //       Container(
+                  //           decoration: BoxDecoration(
+                  //               color: primary,
+                  //               borderRadius: BorderRadius.circular(10)),
+                  //           child: IconButton(
+                  //               onPressed: () {},
+                  //               icon:
+                  //                   const Icon(Icons.add, color: Colors.white)))
+                  //     ]),
+                  const SizedBox(height: 20),
                   GridView.builder(
                       shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
                       gridDelegate:
                           const SliverGridDelegateWithFixedCrossAxisCount(
-                              mainAxisExtent: 70,
+                              mainAxisExtent: 100,
                               crossAxisSpacing: 20,
                               mainAxisSpacing: 20,
                               crossAxisCount: 2),
