841ad07851ffe68c922c115950c1bae539b38651
H-R-S <369haris@gmail.com>
2023-04-28 20:10:26 +0500

    Widget UI updated | UserInfoContainer Widget

--- lib/view/widgets/user_info_container/user_info_container.dart
+++ lib/view/widgets/user_info_container/user_info_container.dart
@@ -22,16 +22,20 @@ class UserInfoContainer extends StatelessWidget {
     return InkWell(
         onTap: onTap,
         child: Container(
+          height: 100,
             padding: const EdgeInsets.all(20),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30), color: primary),
-            child: Row(children: [
+                borderRadius: BorderRadius.circular(20), color: primary),
+            child: Row(
+              children: [
               const CircleAvatar(
                   radius: 30,
                   backgroundColor: Colors.white,
                   child: Icon(Icons.person, color: primary, size: 30)),
               const SizedBox(width: 20),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start, children: [
                 Text(name, style: style.copyWith(fontWeight: FontWeight.bold)),
                 const SizedBox(height: 5),
                 FittedBox(
