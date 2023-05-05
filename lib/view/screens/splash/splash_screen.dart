0b1f360169dc7b6b486b067a54486335cd3f1d4c
H-R-S <369haris@gmail.com>
2023-05-05 21:54:09 +0500

    New screen added | Splash Screen

--- lib/view/screens/splash/splash_screen.dart
+++ lib/view/screens/splash/splash_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import '../../../resources/constants/logos.dart';
+import '../../../services/splash/splash_service.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  SplashServices splashServices = SplashServices();
+
+  @override
+  void initState() {
+    super.initState();
+
+    splashServices.checkAuthentication(context);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Image.asset(iqraLogo, height: 200),
+        ));
   }
 }
