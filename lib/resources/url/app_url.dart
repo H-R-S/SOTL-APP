6363bcb905c1bffb5b9adf272f8c8bf7fcea91d3
H-R-S <369haris@gmail.com>
2023-05-05 21:50:33 +0500

    New EndPoints added | AppURL

--- lib/resources/url/app_url.dart
+++ lib/resources/url/app_url.dart
@@ -1,6 +1,6 @@
 class AppUrl {
 
-  static var baseUrl = "";
+  static var baseUrl = "https://sotl-server.vercel.app";
 
   static var loginEndPoint = "$baseUrl/api/login";
 
@@ -9,4 +9,6 @@ class AppUrl {
   static var getAllUsersEndPoint = "$baseUrl/api/users";
 
   static var updateUserEndPoint = "$baseUrl/api/update";
+
+  static var getAllCoursesEndPoint = "$baseUrl/api/courses";
 }
