a8ba5eb1a885d835d20c98e99c2df02b4cf4fa13
H-R-S <369haris@gmail.com>
2023-05-10 19:04:37 +0500

    Widget UI updated | SlotsDropDown Widget

--- lib/view/widgets/search_drop_down/search_drop_down.dart
+++ lib/view/widgets/search_drop_down/search_drop_down.dart
@@ -79,16 +79,63 @@ class CourseDropDown extends StatelessWidget {
         ]),
       const SizedBox(height: 5),
       Container(
-          padding: const EdgeInsets.only(left: 10, right: 10),
+          padding: const EdgeInsets.only(left: 10, right: 5, top: 5),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               border: Border.all(color: grey)),
           child: DropdownSearch<CourseModel>(
               itemAsString: (CourseModel course) {
-                List<String> slots = course.slots!
-                    .map((slot) => "(${slot.time}) (${slot.location})")
-                    .toList();
-                return "${course.name} ${slots.join('\n\n${course.name} ')}";
+                return "${course.name}";
+              },
+              popupProps: const PopupProps.menu(
+                showSearchBox: true,
+                // showSelectedItems: true,
+              ),
+              items: items,
+              // selectedItem: selectedItem,
+              dropdownDecoratorProps: DropDownDecoratorProps(
+                  dropdownSearchDecoration: InputDecoration(hintText: hint)),
+              onChanged: onChanged))
+    ]);
+  }
+}
+
+class SlotDropDown extends StatelessWidget {
+  final String? header;
+  final bool isRequired;
+  final String hint;
+  final List<Slots> items;
+  final Slots? selectedItem;
+  final Function(Slots?) onChanged;
+
+  const SlotDropDown(
+      {super.key,
+      required this.items,
+      required this.hint,
+      required this.onChanged,
+      this.selectedItem,
+      this.header,
+      this.isRequired = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      if (header != null)
+        Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Text(header!, style: const TextStyle(fontSize: 16)),
+          const SizedBox(width: 3),
+          if (isRequired)
+            const Text("*", style: TextStyle(fontSize: 16, color: Colors.red))
+        ]),
+      const SizedBox(height: 5),
+      Container(
+          padding: const EdgeInsets.only(left: 10, right: 5, top: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: grey)),
+          child: DropdownSearch<Slots>(
+              itemAsString: (Slots slot) {
+                return "(${slot.day} ${slot.time}) (${slot.location})";
               },
               popupProps: const PopupProps.menu(
                 showSearchBox: true,
