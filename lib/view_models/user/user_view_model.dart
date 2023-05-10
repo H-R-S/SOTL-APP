6b9ad56010d6e136f79f62c97f41608fcf18afae
H-R-S <369haris@gmail.com>
2023-05-10 19:05:27 +0500

    View model updated | User View Model

--- lib/view_models/user/user_view_model.dart
+++ lib/view_models/user/user_view_model.dart
@@ -1,8 +1,49 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sotl/view/widgets/snack_bar/my_snack_bar.dart';
 import '../../models/user/user_model.dart';
+import '../../repository/user/user_repository.dart';
 
 class UserViewModel with ChangeNotifier {
+  final _userRepo = UserRepository();
+
+  bool _loading = false;
+
+  bool get loading => _loading;
+
+  setLoading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  Future<void> createUser(BuildContext context, UserModel user, String password,
+      {String? courseId}) async {
+    setLoading(true);
+    getUser().then((value) {
+      final header = {'Authorization': 'Bearer ${value.token}'};
+      print(value.token);
+      _userRepo.createUserApi(header, {
+        "name": user.name,
+        "email": user.email,
+        "password": password,
+        "role": user.role,
+        "campus": user.campus,
+        "department": user.department,
+        "course": [courseId]
+      }).then((value) {
+        setLoading(false);
+        if (value.token != null && value.token != "") {
+          MySnackBar(context, "User Added");
+        } else {
+          MySnackBar(context, "Something went wrong!.");
+        }
+      }).onError((error, stackTrace) {
+        setLoading(false);
+        print(error.toString());
+        MySnackBar(context, error.toString());
+      });
+    });
+  }
 
   Future<bool> saveUser(UserModel user) async {
     final SharedPreferences sp = await SharedPreferences.getInstance();
