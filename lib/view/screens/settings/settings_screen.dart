297fe6ef745834501beb262ba72972814c840338
H-R-S <369haris@gmail.com>
2023-05-12 21:42:10 +0500

    Screen UI updated | Settings Screen

--- lib/view/screens/settings/settings_screen.dart
+++ lib/view/screens/settings/settings_screen.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sotl/view_models/user/user_view_model.dart';
 import '../../../routes/routes_name.dart';
 import '../../../view_models/auth/auth_view_model.dart';
+import '../../../view_models/user/user_view_model.dart';
 import '../../widgets/app_bar/my_app_bar.dart';
 import '../../widgets/settings_container/settings_container.dart';
-import '../../widgets/snack_bar/my_snack_bar.dart';
+import '../../widgets/user_info_container/user_info_container.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   SettingsScreen({super.key});
 
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  TextEditingController nameController = TextEditingController(text: "Name");
+  TextEditingController emailController = TextEditingController(text: "Email");
+  TextEditingController roleController = TextEditingController(text: "Role");
 
   @override
   Widget build(BuildContext context) {
+    final userViewModel = Provider.of<UserViewModel>(context);
+
+    userViewModel.getUser().then((value) {
+      setState(() {
+        nameController.value = nameController.value.copyWith(text: value.name);
+        emailController.value =
+            emailController.value.copyWith(text: value.email);
+        roleController.value = roleController.value
+            .copyWith(text: value.role!.replaceAll("_", " "));
+      });
+    });
+
     final authViewModel = Provider.of<AuthViewModel>(context);
 
     return Scaffold(
@@ -22,29 +43,13 @@ class SettingsScreen extends StatelessWidget {
             padding: const EdgeInsets.only(top: 20),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              // const HeaderTag(title: "Settings"),
-              // const SizedBox(height: 20),
-              // UserInfoContainer(
-              //     onTap: () {
-              //       Navigator.pushNamed(context, RoutesName.profile);
-              //     },
-              //     name: "Dr. Fariha Hayat",
-              //     email: "fariha.hayat@iqra.edu.pk",
-              //     role: "Instructor"),
-              // Container(
-              //   height: 200,
-              //   color: primary,
-              //   child: Padding(
-              //     padding: const EdgeInsets.all(20),
-              //     child: UserInfoContainer(
-              //       onTap: () {
-              //         Navigator.pushNamed(context, RoutesName.profile);
-              //       },
-              //       name: "Dr. Fariha Hayat",
-              //       email: "fariha.hayat@iqra.edu.pk",
-              //       role: "Instructor"),
-              //   ),
-              // ),
+              UserInfoContainer(
+                  onTap: () {
+                    Navigator.pushNamed(context, RoutesName.profile);
+                  },
+                  name: nameController.text,
+                  email: emailController.text,
+                  role: roleController.text),
               SettingsContainer(
                   icon: Icons.visibility_outlined,
                   title: "Change Password",
