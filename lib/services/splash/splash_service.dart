34e708d25b37a0b813e60426408b18f2a63f7e83
H-R-S <369haris@gmail.com>
2023-05-12 21:40:45 +0500

    Services updated | Splash Service

--- lib/services/splash/splash_service.dart
+++ lib/services/splash/splash_service.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '../../models/user/user_model.dart';
 import '../../routes/routes_name.dart';
 import '../../view_models/user/user_view_model.dart';
