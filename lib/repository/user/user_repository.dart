87a7dbc87a14e04d7071db214af1ffad6c469546
H-R-S <369haris@gmail.com>
2023-05-12 21:38:27 +0500

    Add user function updated | User Repository

--- lib/repository/user/user_repository.dart
+++ lib/repository/user/user_repository.dart
@@ -4,12 +4,23 @@ import '../../models/user/user_model.dart';
 import '../../resources/url/app_url.dart';
 
 class UserRepository {
-
   BaseApiServices apiServices = NetworkApiService();
+
+  Future<UserModel> getUserApi(int id) async {
+    try {
+      dynamic response =
+          await apiServices.getApiResponse("${AppUrl.getUserEndPoint}/$id");
+
+      return response = UserModel.fromJson(response);
+    } catch (e) {
+      rethrow;
+    }
+  }
 
   Future<UserModel> createUserApi(dynamic header, data) async {
     try {
-      dynamic response = await apiServices.getPostApiResponseWithHeader(AppUrl.creatUserEndPoint, header, data);
+      dynamic response = await apiServices.getPostApiResponseWithHeader(
+          AppUrl.creatUserEndPoint, header, data);
 
       return response = UserModel.fromJson(response);
     } catch (e) {
