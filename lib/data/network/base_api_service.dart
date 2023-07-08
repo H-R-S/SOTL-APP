b730f2cda0d0052f7f145c723171d45de1c465fa
H-R-S <369haris@gmail.com>
2023-07-08 22:06:54 +0500

    Put Method added | Base Api Service

--- lib/data/network/base_api_service.dart
+++ lib/data/network/base_api_service.dart
@@ -2,6 +2,8 @@ abstract class BaseApiServices {
 
   Future<dynamic> getApiResponse(String url);
 
+  Future<dynamic> getPutApiResponse(String url, dynamic data);
+
   Future<dynamic> getPostApiResponse(String url, dynamic data);
 
   Future<dynamic> getPostApiResponseWithHeader(String url, dynamic header, dynamic data);
