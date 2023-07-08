76c0870a5b81b7165bff3894c3bfbe874737c5c0
H-R-S <369haris@gmail.com>
2023-07-08 22:08:09 +0500

    Model attributes updated | Course Model

--- lib/models/course/course_model.dart
+++ lib/models/course/course_model.dart
@@ -1,5 +1,6 @@
 class CourseModel {
-  String? id;
+  int? id;
+  String? courseCode;
   String? name;
   String? department;
   String? campus;
@@ -7,19 +8,23 @@ class CourseModel {
   bool? isDepthElective;
   int? credits;
   List<Slots>? slots;
+  List<Observations>? observations;
 
   CourseModel(
       {this.id,
+      this.courseCode,
       this.name,
       this.department,
       this.campus,
       this.isElective,
       this.isDepthElective,
       this.credits,
-      this.slots});
+      this.slots,
+      this.observations});
 
   CourseModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
+    courseCode = json['courseCode'];
     name = json['name'];
     department = json['department'];
     campus = json['campus'];
@@ -32,11 +37,18 @@ class CourseModel {
         slots!.add(Slots.fromJson(v));
       });
     }
+    if (json['observations'] != null) {
+      observations = <Observations>[];
+      json['observations'].forEach((v) {
+        observations!.add(Observations.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
+    data['courseCode'] = courseCode;
     data['name'] = name;
     data['department'] = department;
     data['campus'] = campus;
@@ -46,41 +58,47 @@ class CourseModel {
     if (slots != null) {
       data['slots'] = slots!.map((v) => v.toJson()).toList();
     }
+    if (observations != null) {
+      data['observations'] = observations!.map((v) => v.toJson()).toList();
+    }
     return data;
   }
 }
 
 class Slots {
-  String? id;
+  int? id;
+  String? sectionCode;
   String? time;
   String? location;
   String? day;
-  String? courseId;
+  int? courseId;
   int? facultyId;
-  dynamic slotFacultyId;
-  dynamic slotObserverId;
+  dynamic facultyobsId;
+  dynamic observerObsId;
   Faculty? faculty;
 
   Slots(
       {this.id,
+      this.sectionCode,
       this.time,
       this.location,
       this.day,
       this.courseId,
       this.facultyId,
-      this.slotFacultyId,
-      this.slotObserverId,
+      this.facultyobsId,
+      this.observerObsId,
       this.faculty});
 
   Slots.fromJson(Map<String, dynamic> json) {
     id = json['id'];
+    sectionCode = json['sectionCode'];
     time = json['time'];
     location = json['location'];
     day = json['day'];
     courseId = json['courseId'];
     facultyId = json['facultyId'];
-    slotFacultyId = json['slotFacultyId'];
-    slotObserverId = json['slotObserverId'];
+    facultyobsId = json['facultyobsId'];
+    observerObsId = json['observerObsId'];
     faculty =
         json['faculty'] != null ? Faculty.fromJson(json['faculty']) : null;
   }
@@ -88,13 +106,14 @@ class Slots {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
+    data['sectionCode'] = sectionCode;
     data['time'] = time;
     data['location'] = location;
     data['day'] = day;
     data['courseId'] = courseId;
     data['facultyId'] = facultyId;
-    data['slotFacultyId'] = slotFacultyId;
-    data['slotObserverId'] = slotObserverId;
+    data['facultyobsId'] = facultyobsId;
+    data['observerObsId'] = observerObsId;
     if (faculty != null) {
       data['faculty'] = faculty!.toJson();
     }
@@ -117,6 +136,67 @@ class Faculty {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['name'] = name;
     data['email'] = email;
+    return data;
+  }
+}
+
+class Observations {
+  int? id;
+  dynamic starting;
+  dynamic ending;
+  String? observationStatus;
+  int? observationProgress;
+  String? semester;
+  int? observationScore;
+  int? facultyId;
+  int? hodId;
+  int? observerId;
+  int? courseId;
+  String? createdAt;
+
+  Observations(
+      {this.id,
+      this.starting,
+      this.ending,
+      this.observationStatus,
+      this.observationProgress,
+      this.semester,
+      this.observationScore,
+      this.facultyId,
+      this.hodId,
+      this.observerId,
+      this.courseId,
+      this.createdAt});
+
+  Observations.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    starting = json['starting'];
+    ending = json['ending'];
+    observationStatus = json['observationStatus'];
+    observationProgress = json['observationProgress'];
+    semester = json['semester'];
+    observationScore = json['observationScore'];
+    facultyId = json['facultyId'];
+    hodId = json['hodId'];
+    observerId = json['observerId'];
+    courseId = json['courseId'];
+    createdAt = json['createdAt'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['starting'] = starting;
+    data['ending'] = ending;
+    data['observationStatus'] = observationStatus;
+    data['observationProgress'] = observationProgress;
+    data['semester'] = semester;
+    data['observationScore'] = observationScore;
+    data['facultyId'] = facultyId;
+    data['hodId'] = hodId;
+    data['observerId'] = observerId;
+    data['courseId'] = courseId;
+    data['createdAt'] = createdAt;
     return data;
   }
 }
