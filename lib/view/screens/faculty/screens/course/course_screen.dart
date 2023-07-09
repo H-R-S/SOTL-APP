eea5b10e7f37509de17733bdbd55db9e5d5a3643
H-R-S <369haris@gmail.com>
2023-07-09 17:00:52 +0500

    Screen UI Updated | Course Screen

--- lib/view/screens/faculty/screens/course/course_screen.dart
+++ lib/view/screens/faculty/screens/course/course_screen.dart
@@ -1,27 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sotl/data/enums/status.dart';
+import 'package:sotl/view/widgets/loading_indicator/my_loading_indicator.dart';
+import 'package:sotl/view_models/course/course_view_model.dart';
+import 'package:sotl/view_models/user/user_view_model.dart';
 import '../../../../widgets/app_bar/my_app_bar.dart';
 import '../../../../widgets/course_container/course_container.dart';
 
-class FacultyCourseScreen extends StatelessWidget {
-  FacultyCourseScreen({Key? key});
+class FacultyCourseScreen extends StatefulWidget {
+  const FacultyCourseScreen({super.key});
+
+  @override
+  State<FacultyCourseScreen> createState() => _FacultyCourseScreenState();
+}
+
+class _FacultyCourseScreenState extends State<FacultyCourseScreen> {
+  CourseViewModel courseViewModel = CourseViewModel();
+
+  @override
+  void initState() {
+    courseViewModel.getAllCourses();
+    super.initState();
+  }
 
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
+    final userViewModel = Provider.of<UserViewModel>(context);
+
+    userViewModel.getUser().then((value) {
+      debugPrint("uid:${value.id.toString()}");
+    });
+
     return Scaffold(
         appBar: MyAppBar(scaffoldKey, context, title: "Courses"),
         body: Padding(
             padding: const EdgeInsets.all(20),
-            child: Column(children: [
-              CourseContainer(
-                  name: "FINAL YEAR PROJECT - II",
-                  timings: "(Thursday 15:00:00 18:00:00)",
-                  instructorName: "Shearaz Ahmed"),
-              CourseContainer(
-                  name: "PROBABILTY AND STATISTICS",
-                  timings: "(Monday 15:00:00 18:00:00)",
-                  instructorName: "Shearaz Ahmed")
-            ])));
+            child: ChangeNotifierProvider<CourseViewModel>(
+                create: (context) => courseViewModel,
+                builder: (context, snapshot) {
+                  return Consumer<CourseViewModel>(
+                      builder: (context, value, child) {
+                    switch (value.courseList.status) {
+                      case Status.ERROR:
+                        debugPrint(value.courseList.message);
+                        return Container();
+
+                      case Status.COMPLETED:
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: value.courseList.data!.length,
+                            itemBuilder: (context, index) {
+                              final course = value.courseList.data![index];
+
+                              return CourseContainer(
+                                  name: course.name ?? "",
+                                  timings:
+                                      "${course.slots![0].location} (${course.slots![0].time})",
+                                  instructorName:
+                                      course.slots![0].faculty!.name ?? "");
+                            });
+
+                      default:
+                        return const MyLoadingIndicator();
+                    }
+                  });
+                })));
   }
 }
