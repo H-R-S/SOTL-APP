66a4529c3e05dd1eeb2689f27079bad8234f5bf1
H-R-S <369haris@gmail.com>
2023-07-09 16:59:51 +0500

    Screen UI Updated | Admin Screen

--- lib/view/screens/admin/admin.dart
+++ lib/view/screens/admin/admin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../resources/constants/style.dart';
+import 'screens/courses/courses_screen.dart';
 import 'screens/dashboard/dashboard_screen.dart';
 import '../common/screens/observation/observation_screen.dart';
 import '../common/screens/settings/settings_screen.dart';
@@ -7,7 +8,6 @@ import 'screens/users_list/users_list_screen.dart';
 
 class Admin extends StatefulWidget {
   const Admin({super.key});
-
 
   @override
   State<Admin> createState() => _AdminState();
@@ -28,86 +28,59 @@ class _AdminState extends State<Admin> {
     List<Widget> screens = [
       DashboardScreen(),
       UsersListScreen(title: "Search Faculty"),
-      // UsersScreen(),
+      CoursesScreen(),
       ObservationScreen(),
       SettingsScreen()
     ];
 
     return Scaffold(
+        backgroundColor: light,
         // body: screens[selectedIndex],
         body: PageView(
             physics: const NeverScrollableScrollPhysics(),
             controller: controller,
             children: screens),
         bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Styles.scaffoldBgColor,
-          onTap: (index) {
-            setState(() {
-              selectedIndex = index;
-            });
-            controller.animateToPage(selectedIndex,
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.easeOutQuad);
-          },
-          iconSize: 30,
-          selectedItemColor: primary,
-          unselectedItemColor: Colors.grey,
-          currentIndex: selectedIndex,
-          items: [
-            BottomNavigationBarItem(
-                icon: selectedIndex == 0
-                    ? const Icon(Icons.home)
-                    : const Icon(Icons.home_outlined),
-                label: 'Admin'),
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.search), label: 'Search'),
-            const BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.calendar_today_outlined,
-                ),
-                label: 'Observations'),
-            BottomNavigationBarItem(
-                icon: selectedIndex == 3
-                    ? const Icon(Icons.settings)
-                    : const Icon(Icons.settings_outlined),
-                label: 'Settings'),
-          ],
-          // // barItems: [
-          //   BarItem(icon: Icons.space_dashboard_outlined, title: 'Dashboard'),
-          //   BarItem(icon: Icons.person_add_alt, title: 'Users'),
-          //   BarItem(
-          //       icon: Icons.calendar_today_outlined, title: 'Observations'),
-          //   BarItem(
-          //     icon: Icons.settings_outlined,
-          //     title: 'Settings',
-          //   )
-          // ]
-        )
-        // bottomNavigationBar: BottomNavigationBar(
-        //     elevation: 0,
-        //     backgroundColor: primary,
-        //     currentIndex: currentIndex,
-        //     selectedItemColor: Colors.white,
-        //     iconSize: 30,
-        //     unselectedItemColor: Colors.white30,
-        //     showSelectedLabels: false,
-        //     showUnselectedLabels: false,
-        //     type: BottomNavigationBarType.fixed,
-        //     onTap: (value) {
-        //       setState(() {
-        //         currentIndex = value;
-        //       });
-        //     },
-        //     items: const [
-        //       BottomNavigationBarItem(icon: Icon(Icons.dashboard_outlined), label: "Dashboard"),
-        //       BottomNavigationBarItem(
-        //           icon: Icon(Icons.person_add_alt), label: "Users"),
-        //       BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: "Observations"),
-        //       BottomNavigationBarItem(
-
-        //           icon: Icon(Icons.settings_outlined), label: "Settings")
-        //     ])
-        );
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Styles.scaffoldBgColor,
+            onTap: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+              controller.animateToPage(selectedIndex,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeOutQuad);
+            },
+            iconSize: 30,
+            selectedItemColor: primary,
+            unselectedItemColor: Colors.grey,
+            currentIndex: selectedIndex,
+            items: [
+              BottomNavigationBarItem(
+                  icon: selectedIndex == 0
+                      ? const Icon(Icons.home)
+                      : const Icon(Icons.home_outlined),
+                  label: 'Admin'),
+              BottomNavigationBarItem(
+                  icon: Icon(selectedIndex == 1
+                      ? Icons.person
+                      : Icons.person_outlined),
+                  label: 'Users'),
+              BottomNavigationBarItem(
+                  icon: Icon(selectedIndex == 2
+                      ? Icons.file_copy
+                      : Icons.file_copy_outlined),
+                  label: 'Course'),
+              BottomNavigationBarItem(
+                  icon: Icon(selectedIndex == 3
+                      ? Icons.calendar_month
+                      : Icons.calendar_today_outlined),
+                  label: 'Observations'),
+              BottomNavigationBarItem(
+                  icon: selectedIndex == 4
+                      ? const Icon(Icons.settings)
+                      : const Icon(Icons.settings_outlined),
+                  label: 'Settings')
+            ]));
   }
 }
