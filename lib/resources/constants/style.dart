c6f88e01eca67a7db0ca2e2384f5ec48a7390a01
H-R-S <369haris@gmail.com>
2023-07-17 12:45:22 +0500

    New color's added | Style Constants

--- lib/resources/constants/style.dart
+++ lib/resources/constants/style.dart
@@ -7,25 +7,17 @@ class Styles {
   static Color whiteColor = HexColor('#FFFFFF');
   static Color scaffoldBgColor = HexColor('#F0F2FF');
 
-  static TextStyle h1 = const TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.bold
-  );
+  static TextStyle h1 =
+      const TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
 
-  static TextStyle h2 = const TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w600
-  );
+  static TextStyle h2 =
+      const TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
 
-  static TextStyle h3 = const TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w500
-  );
+  static TextStyle h3 =
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
 
-  static TextStyle subHeading = const TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.normal
-  );
+  static TextStyle subHeading =
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.normal);
 }
 
 const Color primary = Color(0xff040B5B);
@@ -33,4 +25,4 @@ const Color active = Color(0xffFCC43E);
 const Color secondary = Color(0xff4AD6EF);
 const Color light = Color(0xffF0F2FF);
 const Color grey = Color(0xffA098AE);
-
+const Color dark = Color(0xff222831);
