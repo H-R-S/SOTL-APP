3f2bbd0cae48121cd8229fc52d944cce8271a9c3
H-R-S <369haris@gmail.com>
2023-07-17 12:49:05 +0500

    Screen UI updated | Add User Screen

--- lib/view/screens/admin/screens/add_user/add_user_screen.dart
+++ lib/view/screens/admin/screens/add_user/add_user_screen.dart
@@ -3,11 +3,16 @@ import 'package:provider/provider.dart';
 import '../../../../../data/enums/campus.dart';
 import '../../../../../data/enums/departments.dart';
 import '../../../../../data/enums/roles.dart';
+import '../../../../../data/enums/status.dart';
+import '../../../../../models/course/course_model.dart';
+import '../../../../../models/user/user_model.dart';
 import '../../../../../resources/validator/validator.dart';
 import '../../../../../view_models/course/course_view_model.dart';
 import '../../../../../view_models/user/user_view_model.dart';
 import '../../../../widgets/app_bar/my_app_bar.dart';
 import '../../../../widgets/button/my_elevated_button.dart';
+import '../../../../widgets/loading_indicator/my_loading_indicator.dart';
+import '../../../../widgets/search_drop_down/search_drop_down.dart';
 import '../../../../widgets/text_field/my_text_field.dart';
 
 class AddUserScreen extends StatefulWidget {
@@ -22,9 +27,18 @@ class AddUserScreen extends StatefulWidget {
 class _AddUserScreenState extends State<AddUserScreen> {
   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
+  CourseViewModel courseViewModel = CourseViewModel();
+
   final formKey = GlobalKey<FormState>();
 
-  CourseViewModel courseViewModel = CourseViewModel();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController campusController = TextEditingController();
+  TextEditingController departmentController = TextEditingController();
+  TextEditingController roleController = TextEditingController();
+  TextEditingController courseIdController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
 
   @override
   void initState() {
@@ -32,106 +46,193 @@ class _AddUserScreenState extends State<AddUserScreen> {
     super.initState();
   }
 
-  TextEditingController nameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController campusController = TextEditingController();
-  TextEditingController departmentController = TextEditingController();
-  TextEditingController roleController = TextEditingController();
-  TextEditingController courseController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController confirmPasswordController = TextEditingController();
+  List<String> courseIdList = [];
 
   @override
   Widget build(BuildContext context) {
     final userViewModel = Provider.of<UserViewModel>(context);
 
-    return Scaffold(
-        appBar: MyAppBar(scaffoldKey, context, title: "Add ${widget.title}"),
-        body: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-                child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(children: [
-                      MyTextField(
-                          header: "Full Name",
-                          isRequired: true,
-                          validator: (value) =>
-                              Validator.validateForm(value, "Full Name"),
-                          controller: nameController,
-                          hint: "Full Name"),
-                      MyTextField(
-                          header: "Campus",
-                          isRequired: true,
-                          isReadable: true,
-                          dropDownList: Campus.values
-                              .map((i) => i.toString().split('.').last)
-                              .toList(),
-                          validator: (value) =>
-                              Validator.validateForm(value, "Full Name"),
-                          controller: emailController,
-                          hint: "Campus"),
-                      MyTextField(
-                          header: "Department",
-                          isRequired: true,
-                          isReadable: true,
-                          dropDownList: Departments.values
-                              .map((i) => i.toString().split('.').last)
-                              .toList(),
-                          validator: (value) =>
-                              Validator.validateForm(value, "Department"),
-                          controller: departmentController,
-                          hint: "Department"),
-                      MyTextField(
-                          header: "Role",
-                          isRequired: true,
-                          isReadable: true,
-                          dropDownList: Roles.values
-                              .map((i) => i.toString().split('.').last)
-                              .toList(),
-                          validator: (value) =>
-                              Validator.validateForm(value, "Role"),
-                          controller: roleController,
-                          hint: "Role"),
-                      MyTextField(
-                          header: "Password",
-                          isRequired: true,
-                          validator: (value) =>
-                              Validator.validatePassword(value),
-                          controller: passwordController,
-                          isPassword: true,
-                          isVisible: true,
-                          hint: "Password"),
-                      MyTextField(
-                          header: "Confirm Password",
-                          isRequired: true,
-                          validator: (value) =>
-                              Validator.validateConfirmPassword(
-                                  value, passwordController.text.trim()),
-                          controller: confirmPasswordController,
-                          isPassword: true,
-                          isVisible: true,
-                          hint: "Confirm Password"),
-                      const SizedBox(height: 40),
-                      MyElevatedButton(
-                        isLoading: userViewModel.loading,
-                          title: "Add ${widget.title}",
-                          onTap: () {
-                            final isValidate = formKey.currentState!.validate();
+    return GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+            appBar: MyAppBar(scaffoldKey, context,
+                title: "Create User", isBack: true),
+            body: SingleChildScrollView(
+                child: Form(
+                    key: formKey,
+                    child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: ChangeNotifierProvider<CourseViewModel>(
+                            create: (context) => courseViewModel,
+                            child: Consumer<CourseViewModel>(
+                                builder: (context, value, child) {
+                              switch (value.courseList.status) {
+                                case Status.ERROR:
+                                  debugPrint(value.courseList.message);
+                                  return Container();
 
-                            if (isValidate) {
-                              // userViewModel.createUser(
-                              //     context,
-                              //     UserModel(
-                              //         name: nameController.text.trim(),
-                              //         email: emailController.text.trim(),
-                              //         campus: campusController.text,
-                              //         department: departmentController.text,
-                              //         role: roleController.text),
-                              //     passwordController.text.trim(),
-                              //     courseId: []);
-                            }
-                          })
-                    ])))));
+                                case Status.COMPLETED:
+                                  final course = value.courseList.data!;
+                                  // .where((course) => course.slots!
+                                  //     .every((slot) => slot.facultyId == null))
+                                  // .toList();
+
+                                  final slots = <Slots>[];
+
+                                  return Column(children: [
+                                    MyTextField(
+                                        header: "Full Name",
+                                        isRequired: true,
+                                        validator: (value) =>
+                                            Validator.validateForm(
+                                                value, "Full Name"),
+                                        controller: nameController,
+                                        hint: "Full Name"),
+                                    MyTextField(
+                                        header: "Email",
+                                        isRequired: true,
+                                        validator: (value) =>
+                                            Validator.validateEmail(value),
+                                        controller: emailController,
+                                        hint: "Email"),
+                                    MyTextField(
+                                        header: "Campus",
+                                        isRequired: true,
+                                        isReadable: true,
+                                        dropDownList: Campus.values
+                                            .map((i) =>
+                                                i.toString().split('.').last)
+                                            .toList(),
+                                        validator: (value) =>
+                                            Validator.validateForm(
+                                                value, "Full Name"),
+                                        controller: campusController,
+                                        hint: "Campus"),
+                                    MyTextField(
+                                        header: "Department",
+                                        isRequired: true,
+                                        isReadable: true,
+                                        dropDownList: Departments.values
+                                            .map((i) =>
+                                                i.toString().split('.').last)
+                                            .toList(),
+                                        validator: (value) =>
+                                            Validator.validateForm(
+                                                value, "Department"),
+                                        controller: departmentController,
+                                        hint: "Department"),
+                                    MyTextField(
+                                        header: "Role",
+                                        isRequired: true,
+                                        isReadable: true,
+                                        onChanged: (value) {
+                                          setState(() {
+                                            
+                                          });
+                                        },
+                                        dropDownList: Roles.values
+                                            .map((i) =>
+                                                i.toString().split('.').last)
+                                            .toList(),
+                                        validator: (value) =>
+                                            Validator.validateForm(
+                                                value, "Role"),
+                                        controller: roleController,
+                                        hint: "Role"),
+                                    const SizedBox(height: 10),
+                                    if (roleController.text == "Faculty")
+                                    CourseDropDown(
+                                        header: "Courses",
+                                        isRequired: true,
+                                        items: course,
+                                        hint: "Select Course",
+                                        onChanged: (value) {
+                                          slots.clear();
+                                          slots.addAll(value!.slots!.where(
+                                              (slot) =>
+                                                  slot.facultyId == null));
+                                          debugPrint(value.slots.toString());
+                                        }),
+                                    const SizedBox(height: 15),
+                                                                        if (roleController.text == "Faculty")
+
+                                    SlotDropDown(
+                                        header: "Slots",
+                                        isRequired: true,
+                                        items: slots,
+                                        hint: "Select Slots",
+                                        onChanged: (value) {
+                                          courseIdList
+                                              .add(value!.id.toString());
+                                        }),
+                                    MyTextField(
+                                        header: "Password",
+                                        isRequired: true,
+                                        validator: (value) =>
+                                            Validator.validatePassword(value),
+                                        controller: passwordController,
+                                        isPassword: true,
+                                        isVisible: true,
+                                        hint: "Password"),
+                                    MyTextField(
+                                        header: "Confirm Password",
+                                        isRequired: true,
+                                        validator: (value) =>
+                                            Validator.validateConfirmPassword(
+                                                value,
+                                                passwordController.text.trim()),
+                                        controller: confirmPasswordController,
+                                        isPassword: true,
+                                        isVisible: true,
+                                        hint: "Confirm Password"),
+                                    const SizedBox(height: 40),
+                                    MyElevatedButton(
+                                        isLoading: userViewModel.loading,
+                                        title: "Add Faculty",
+                                        onTap: () {
+                                          final isValidate =
+                                              formKey.currentState!.validate();
+
+                                          if (isValidate) {
+                                            userViewModel
+                                                .createUser(
+                                                    context,
+                                                    UserModel(
+                                                        name: nameController
+                                                            .text
+                                                            .trim(),
+                                                        email: emailController
+                                                            .text
+                                                            .trim(),
+                                                        campus: campusController
+                                                            .text,
+                                                        department:
+                                                            departmentController
+                                                                .text,
+                                                        role: roleController
+                                                            .text),
+                                                    password: passwordController
+                                                        .text
+                                                        .trim(),
+                                                    courseId: courseIdList)
+                                                .then((value) {
+                                              nameController.clear();
+                                              emailController.clear();
+                                              campusController.clear();
+                                              departmentController.clear();
+                                              roleController.clear();
+                                              courseIdController.clear();
+                                              passwordController.clear();
+                                              confirmPasswordController.clear();
+                                            });
+                                          }
+                                        }),
+                                    const SizedBox(height: 40)
+                                  ]);
+
+                                default:
+                                  return const MyLoadingIndicator();
+                              }
+                            })))))));
   }
 }
