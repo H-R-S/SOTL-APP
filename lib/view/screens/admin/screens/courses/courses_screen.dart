696a1f99c0515cfc703d41158d99859d728e8e5b
H-R-S <369haris@gmail.com>
2023-07-17 13:09:18 +0500

    Screen UI updated | Courses Screen

--- lib/view/screens/admin/screens/courses/courses_screen.dart
+++ lib/view/screens/admin/screens/courses/courses_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sotl/view/screens/admin/screens/assigned_course/assigned_course_screen.dart';
-import 'package:sotl/view/widgets/button/my_elevated_button.dart';
 import '../../../../../data/enums/status.dart';
-import '../../../../../resources/constants/style.dart';
 import '../../../../../view_models/course/course_view_model.dart';
 import '../../../../widgets/app_bar/my_app_bar.dart';
+import '../../../../widgets/button/my_elevated_button.dart';
 import '../../../../widgets/course_container/course_container.dart';
 import '../../../../widgets/loading_indicator/my_loading_indicator.dart';
 import '../../../../widgets/search_bar/my_search_bar.dart';
 import '../add_course/add_course_screen.dart';
+import '../assigned_course/assigned_course_screen.dart';
 
 class CoursesScreen extends StatefulWidget {
   const CoursesScreen({super.key});
@@ -33,48 +32,34 @@ class _CoursesScreenState extends State<CoursesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: light,
         appBar: MyAppBar(scaffoldKey, context,
-            title: "Courses",
-            actionData: Container(
-                margin: const EdgeInsets.only(right: 10),
-                padding: const EdgeInsets.all(3),
-                decoration:
-                    const BoxDecoration(color: primary, shape: BoxShape.circle),
-                child: InkWell(
-                    onTap: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (context) => Padding(
-                                padding: const EdgeInsets.all(20),
-                                child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      MyElevatedButton(
-                                          title: "Add Course",
-                                          onTap: () {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        AddCourseScreen()));
-                                          }),
-                                      const SizedBox(height: 20),
-                                      MyElevatedButton(
-                                          title: "Assigned Course",
-                                          onTap: () {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        const AssignedCourseScreen()));
-                                          }),
-                                      const SizedBox(height: 40)
-                                    ]),
-                              ));
-                    },
-                    child: const Center(
-                        child: Icon(Icons.add, color: Colors.white))))),
+            title: "Courses", actionIcon: Icons.add, onTapAction: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) => Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    MyElevatedButton(
+                        title: "Add Course",
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AddCourseScreen()));
+                        }),
+                    const SizedBox(height: 20),
+                    MyElevatedButton(
+                        title: "Assigned Course",
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const AssignedCourseScreen()));
+                        }),
+                    const SizedBox(height: 40)
+                  ])));
+        }),
         body: SingleChildScrollView(
             child: Padding(
                 padding: const EdgeInsets.all(20),
