ec48f0e4dd5f9960b88e0de49c9cf6dbe4b35b59
H-R-S <369haris@gmail.com>
2023-08-08 12:33:25 -0700

    Widget UI Updated | Course Card Widget

---
+++ lib/view/widgets/course_card/course_skeleton_card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:sotl/view/widgets/skeleton/skeleton.dart';
+
+class CourseSkeletonCard extends StatelessWidget {
+  const CourseSkeletonCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Theme.of(context).primaryColor.withOpacity(0.1),
+          )),
+      child: const Row(children: [
+        CircleSkeleton(size: 60),
+        SizedBox(width: 10),
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Skeleton(width: 200),
+          SizedBox(height: 10),
+          Skeleton(width: 100),
+          SizedBox(height: 10),
+          Skeleton(width: 150)
+        ]),
+      ]),
+    );
+  }
+}
